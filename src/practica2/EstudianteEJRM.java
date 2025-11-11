2d49308b719a5363c30eef422fa06c2e5c92a0a9
ErnestoRM06 <reynamonnrrealernestojosue@gmail.com>
2025-11-11 17:58:25 -0600

    Practica 2

--- src/practica2/EstudianteEJRM.java
+++ src/practica2/EstudianteEJRM.java
@@ -2,51 +2,11 @@ package practica2;
 
 
 public class EstudianteEJRM {
-    private String nombre;
-    private int matricula;
-    private int edad;
-    private String carrera;
-    private int semestreActual;
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public String getCarrera() {
-        return carrera;
-    }
-
-    public int getSemestreActual() {
-        return semestreActual;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public void setCarrera(String carrera) {
-        this.carrera = carrera;
-    }
-
-    public void setSemestreActual(int semestreActual) {
-        this.semestreActual = semestreActual;
-    }
+    String nombre;
+    int matricula;
+    int edad;
+    String carrera;
+    int semestreActual;
 
     public EstudianteEJRM(String nombre, int matricula, int edad, String carrera, int semestreActual) {
         this.nombre = nombre;
@@ -66,6 +26,23 @@ public class EstudianteEJRM {
         this.matricula = matricula;
         this.carrera = carrera;
         this.semestreActual = semestreActual;
+    }
+    
+    public void mostrarInfo() {
+        System.out.println("Estudiante: " + nombre + ", edad: " + edad + ". Matricula: " + matricula + ", carrera: " + carrera + ", semestre actual: " + semestreActual + ".");
+    }
+    public void aumentarEdad() {
+        edad++;
+    }  
+    public void avanzarSemestre() {
+        semestreActual++;
+    }
+    public boolean esMayorDeEdad() {
+        return edad >= 18;
+    }
+    public void cambiarCarrera(String nuevaCarrera) {
+        carrera = nuevaCarrera;
+        semestreActual = 1;
     }
     
 }
