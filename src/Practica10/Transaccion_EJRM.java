676b9a3e539bbd518dc7c61a797d988adbe4f651
ErnestoRM06 <reynamonnrrealernestojosue@gmail.com>
2025-11-15 10:35:56 -0600

    Practica

---
+++ src/Practica10/Transaccion_EJRM.java
@@ -0,0 +1,12 @@
+package Practica10;
+
+public class Transaccion_EJRM {
+
+    public final String tipo;
+    public final int monto;
+
+    public Transaccion_EJRM(String tipo, int monto) {
+        this.tipo = tipo;
+        this.monto = monto;
+    }
+}
